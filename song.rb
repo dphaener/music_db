74fda8ad95acf1d6f9cae4413d6c461fb86ad840
Darin Haener <dphaener@gmail.com>
2013-08-29 21:06:52 -0700

    Added YAML and dump methods

--- song.rb
+++ song.rb
@@ -1,4 +1,36 @@
+require 'yaml'
+
 class Song
+  
   attr_accessor :title, :artist, :album, :track, :notes
+  
+  def to_s
+    output = "Title: #{@artist}\n"
+    output << "Artist: #{@artist}\n"
+    output << "Album: #{@album}\n"
+    output << "Track: #{@track}\n"
+    output << "Notes: #{@notes}\n"
+    
+    output
+  end
+  
+  def to_yaml
+    the_song = {
+      title: @title,
+      artist: @artist,
+      album: @album,
+      track: @track,
+      notes: @notes
+    }
+    
+    YAML.dump(the_song)
+  end
+  
+  def dump(filename=nil)
+    filename ||= @title
+    filename ||= 'unknown'
+    
+    File.write("#{filename}.yml", to_yaml)
+  end
 end
  
