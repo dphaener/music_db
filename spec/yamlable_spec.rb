2b79ad5f0b8c6e606be57900fe11474a4f7b3751
Darin Haener <dphaener@gmail.com>
2013-09-10 16:18:59 -0700

    Added testing for error in to_yaml. Can't get test for file dump to work...

--- spec/yamlable_spec.rb
+++ spec/yamlable_spec.rb
@@ -10,14 +10,43 @@ describe YAMLable do
     end
   end
   
-  before :each do
-    @test_class = TestClass.new( { name: "Keller Williams" } )
-    @test_class.extend(YAMLable)
+  class EmptyClass
   end
   
   describe "#to_yaml" do
-    it "takes attributes and returns them in YAML format" do
-      expect(@test_class.to_yaml).to eq("---\n:name: Keller Williams\n")
+    context "Has attributes" do
+      before do
+        @test_class = TestClass.new( { name: "Keller Williams" } )
+        @test_class.extend(YAMLable)
+      end
+      it "takes attributes and returns them in YAML format" do
+        expect(@test_class.to_yaml).to eq("---\n:name: Keller Williams\n")
+      end
+    end
+    
+    context "Doesn't have attributes" do
+      before do
+        @empty_class = EmptyClass.new
+        @empty_class.extend(YAMLable)
+      end
+      it "raises an exception when no attributes are passed" do
+        expect { @empty_class.to_yaml }.to raise_error(RuntimeError, "Hey, #{@empty_class.class} needs some attributes!")
+      end
     end
   end
+  
+  describe "#dump" do
+    
+    before do
+      @test_class = TestClass.new( { name: "Keller Williams" } )
+      @test_class.extend(YAMLable)
+      @file = double(@test_class.dump)
+      allow(@file).to receive(:write)
+    end
+    
+    it "writes to a file with a yaml object" do 
+      @file.should_receive(:write).with("unkown.yml", "---\n:name: Keller Williams\n")
+    end
+  end
+  
 end
